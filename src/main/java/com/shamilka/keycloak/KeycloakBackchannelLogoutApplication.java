46e4e47eb1c5b8a8205808540672b8291c274525
shamilka <sham.gurusinghe@gmail.com>
2023-03-22 19:37:33 +0530

    first commit

---
+++ src/main/java/com/shamilka/keycloak/KeycloakBackchannelLogoutApplication.java
@@ -0,0 +1,13 @@
+package com.shamilka.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakBackchannelLogoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakBackchannelLogoutApplication.class, args);
+	}
+
+}
